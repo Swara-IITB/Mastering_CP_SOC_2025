57c89e7fb4bd033895e3ad9c64a394bbbcb8f063
Swara-IITB <swaratakale21@gmail.com>
2025-06-27 02:05:20 +0530

    ..

---
+++ Templates/QuickSelect.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll QuickSelect(vector<ll> v, ll k){
+    if(v.size()==1) return v[0];
+    ll x = v[0];
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+}
